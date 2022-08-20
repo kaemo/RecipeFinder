5b551782b2f537e2efd1bddc5c3eb832424cf4d2
kaemo <kaemo@wp.pl>
2022-08-20 18:37:57 +0200

    * recycler view card layout redesigned * navbar added

--- app/src/main/java/pl/kaemo/recipefinder/ui/recipesListActivity/RecipesListActivity.kt
+++ app/src/main/java/pl/kaemo/recipefinder/ui/recipesListActivity/RecipesListActivity.kt
@@ -2,6 +2,8 @@ package pl.kaemo.recipefinder.ui.recipesListActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +16,9 @@ class RecipesListActivity : AppCompatActivity() {
     private lateinit var adapter: RecipesListAdapter
 
     private lateinit var recyclerViewId: RecyclerView
+    private lateinit var tooltipId: ImageButton
+    private lateinit var sortButtonId: ImageButton
+    private lateinit var moreButtonId: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +27,29 @@ class RecipesListActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(RecipesListViewModel::class.java)
 
         recyclerViewId = findViewById<RecyclerView>(R.id.activity_recipes_list_xml_recyclerview)
+        tooltipId = findViewById(R.id.activity_recipes_list_xml_tooltip)
+        sortButtonId = findViewById(R.id.activity_recipes_list_xml_sort_button)
+        moreButtonId = findViewById(R.id.activity_recipes_list_xml_more_button)
+
+        initRecyclerview()
+
+        tooltipId.setOnClickListener {
+            Toast.makeText(this, "Dialogbox not implemented yet!", Toast.LENGTH_SHORT).show()
+        }
+
+        sortButtonId.setOnClickListener {
+            Toast.makeText(this, "Sorting not implemented yet!", Toast.LENGTH_SHORT).show()
+        }
+
+        moreButtonId.setOnClickListener {
+            Toast.makeText(this, "Settings not implemented yet!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun initRecyclerview(){
         recyclerViewId.layoutManager = LinearLayoutManager(this)
         adapter = RecipesListAdapter()
         recyclerViewId.adapter = adapter
-
     }
 }
--- app/src/main/java/pl/kaemo/recipefinder/ui/recipesListActivity/RecipesListAdapter.kt
+++ app/src/main/java/pl/kaemo/recipefinder/ui/recipesListActivity/RecipesListAdapter.kt
@@ -1,5 +1,6 @@
 package pl.kaemo.recipefinder.ui.recipesListActivity
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -30,15 +31,25 @@ class RecipesListAdapter : RecyclerView.Adapter<RecipesListAdapter.RecipesListVi
             holder.itemView.findViewById(R.id.activity_recipes_list_recyclerview_card_image_view)
         val cardTitle: TextView =
             holder.itemView.findViewById(R.id.activity_recipes_list_recyclerview_card_ingredient_title)
-        val cardSummary: TextView =
-            holder.itemView.findViewById(R.id.activity_recipes_list_recyclerview_card_ingredient_summary)
-
-        cardTitle.text = FakeDataBase.recipeTitle[position]
-        cardSummary.text = FakeDataBase.recipeSummary[position]
+        val cardMissingIngredients: TextView =
+            holder.itemView.findViewById(R.id.activity_recipes_list_recyclerview_card_ingredients_missing)
+        val cardInfo: TextView =
+            holder.itemView.findViewById(R.id.activity_recipes_list_recyclerview_card_ingredients_info)
 
         Glide.with(holder.view.context)
             .load(FakeDataBase.imageUlr[position])
             .into(cardImage)
+
+        cardTitle.text = FakeDataBase.recipeTitle[position]
+
+        if (FakeDataBase.missedIngredientsOriginal[position] == null){
+            cardInfo.text = holder.view.context.getString(R.string.recipes_list_adapter_ingredients_ok)
+            cardInfo.setTextColor(Color.parseColor("#377D3F"))
+        } else {
+            cardInfo.text = holder.view.context.getString(R.string.recipes_list_adapter_ingredients_nok)
+        }
+
+        cardMissingIngredients.text = FakeDataBase.missedIngredientsOriginal[position]
 
         holder.itemView.findViewById<View>(R.id.activity_recipes_list_recyclerview_card_view)
             .setOnClickListener {
@@ -48,6 +59,8 @@ class RecipesListAdapter : RecyclerView.Adapter<RecipesListAdapter.RecipesListVi
                     Toast.LENGTH_SHORT
                 ).show()
             }
+
+
     }
 
     class RecipesListViewHolder(val view: View) : RecyclerView.ViewHolder(view)
