12f488361752e4461a69b0ce2fd71b4357f76eae
kaemo <kaemo@wp.pl>
2022-08-20 19:56:30 +0200

    hotfix

--- app/src/main/java/pl/kaemo/recipefinder/ui/recipesListActivity/RecipesListActivity.kt
+++ app/src/main/java/pl/kaemo/recipefinder/ui/recipesListActivity/RecipesListActivity.kt
@@ -1,9 +1,7 @@
 package pl.kaemo.recipefinder.ui.recipesListActivity
 
-import android.app.AlertDialog
 import android.os.Bundle
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -27,7 +25,7 @@ class RecipesListActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(RecipesListViewModel::class.java)
 
-        recyclerViewId = findViewById<RecyclerView>(R.id.activity_recipes_list_xml_recyclerview)
+        recyclerViewId = findViewById(R.id.activity_recipes_list_xml_recyclerview)
         tooltipId = findViewById(R.id.activity_recipes_list_xml_tooltip)
         sortButtonId = findViewById(R.id.activity_recipes_list_xml_sort_button)
         moreButtonId = findViewById(R.id.activity_recipes_list_xml_more_button)
@@ -35,15 +33,15 @@ class RecipesListActivity : AppCompatActivity() {
         initRecyclerview()
 
         tooltipId.setOnClickListener {
-            showDialog()
+            viewModel.showDialog(this)
         }
 
         sortButtonId.setOnClickListener {
-            Toast.makeText(this, "Sorting not implemented yet!", Toast.LENGTH_SHORT).show()
+            viewModel.toast(this, "Sorting not implemented yet!")
         }
 
         moreButtonId.setOnClickListener {
-            Toast.makeText(this, "Settings not implemented yet!", Toast.LENGTH_SHORT).show()
+            viewModel.toast(this, "Settings not implemented yet!")
         }
 
     }
@@ -52,16 +50,5 @@ class RecipesListActivity : AppCompatActivity() {
         recyclerViewId.layoutManager = LinearLayoutManager(this)
         adapter = RecipesListAdapter()
         recyclerViewId.adapter = adapter
-    }
-
-    private fun showDialog() {
-        val builder:AlertDialog.Builder = AlertDialog.Builder(this)
-        builder.setIcon(R.drawable.ic_baseline_info_24)
-        builder.setTitle("Your plan: FREE")
-        builder.setMessage(R.string.recipes_list_activity_dialog_message)
-        builder.setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
-
-        val alertDialog: AlertDialog = builder.create()
-        alertDialog.show()
     }
 }
--- app/src/main/java/pl/kaemo/recipefinder/ui/recipesListActivity/RecipesListViewModel.kt
+++ app/src/main/java/pl/kaemo/recipefinder/ui/recipesListActivity/RecipesListViewModel.kt
@@ -1,7 +1,25 @@
 package pl.kaemo.recipefinder.ui.recipesListActivity
 
+import android.app.AlertDialog
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import pl.kaemo.recipefinder.R
 
 class RecipesListViewModel: ViewModel() {
 
+    fun showDialog(context: Context) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+        builder.setIcon(R.drawable.ic_baseline_info_24)
+        builder.setTitle("Your plan: FREE")
+        builder.setMessage(R.string.recipes_list_activity_dialog_message)
+        builder.setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    fun toast(context: Context, text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
 }
