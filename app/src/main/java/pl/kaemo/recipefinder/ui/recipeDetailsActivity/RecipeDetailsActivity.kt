0d9d09eb44d7233ba1f34f88de87a15dcf89f014
kaemo <kaemo@wp.pl>
2022-09-19 08:37:33 +0200

    spinner improved

--- app/src/main/java/pl/kaemo/recipefinder/ui/recipeDetailsActivity/RecipeDetailsActivity.kt
+++ app/src/main/java/pl/kaemo/recipefinder/ui/recipeDetailsActivity/RecipeDetailsActivity.kt
@@ -1,6 +1,10 @@
 package pl.kaemo.recipefinder.ui.recipeDetailsActivity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -33,6 +37,32 @@ class RecipeDetailsActivity : AppCompatActivity() {
 
         loadRecipeImage()
         observeUiMessages()
+
+        val servings: List<String> =
+            listOf("0,5 serving", "1 serving", "2 servings", "3 servings", "set custom")
+        val spinner = findViewById<Spinner>(R.id.spinner_servings)
+        val adapter =
+            ArrayAdapter(this, R.layout.spinner_servings_item, R.id.spinner_text_servings, servings)
+        adapter.setDropDownViewResource(R.layout.spinner_servings_dropdown_item)
+        spinner.adapter = adapter
+        spinner.setSelection(2, true) //clean architecture?
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                logger.log(spinner.selectedItem.toString())
+                viewModel.onServingsClicked()
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>?) {
+
+            }
+
+        }
 
     }
 
